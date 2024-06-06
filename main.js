10aaa3993e91f5e0e61fcff993b52863b2502dae
tyrica747 <tyrica747@gmail.com>
2024-06-06 01:29:55 -0700

    index

---
+++ main.js
@@ -0,0 +1,8 @@
+import Typed from 'typed.js';
+var typed=new typed (".text", {
+    String:["innovative 3D game", "built using raycasting technology", "SDL2 for this project"],
+    typedSpeed:100,
+    backSpedd:100,
+    backDelay:1000,
+    loop:true,
+})
